6696a01add03275457cd1dc7c7da373832b93731
JRon103 <villacruzjulianron@hotmail.com>
2023-10-27 07:30:48 -0600

    Se agrego clase atributos

---
+++ CompiladorApp/src/compilador/Atributos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compilador;
+
+/**
+ *
+ * @author villa
+ */
+public class Atributos {
+    
+    String tipo;
+    String tipoAux;
+}
