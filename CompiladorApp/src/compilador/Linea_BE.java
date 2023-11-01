84732f11913a7edde7d9313490cb7d4c117de663
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2023-10-31 20:56:00 -0600

    Documentacion de las acciones semanticas

--- CompiladorApp/src/compilador/Linea_BE.java
+++ CompiladorApp/src/compilador/Linea_BE.java
@@ -3,7 +3,7 @@
  *:                     INGENIERIA EN SISTEMAS COMPUTACIONALES
  *:                         LENGUAJES Y AUTOMATAS II           
  *: 
- *:                  SEMESTRE: AGO-DIC/2013     HORA: 12-13 HRS
+ *:        SEMESTRE: ______________            HORA: ______________ HRS
  *:                                   
  *:               
  *:    # Clase que representa una localidad del Buffer de Entrada
@@ -12,7 +12,7 @@
  *: Archivo       : Linea_BE.java
  *: Autor         : Fernando Gil  
  *:
- *: Fecha         : 03/Octubre/2013
+ *: Fecha         : 03/SEP/2014
  *: Compilador    : Java JDK 7
  *: Descripción   : Clase en la que se definiran el contructor con los parametros necesarios para 
  *:                 nuestra linea del buffer de entrada asi como se definiran tambien las propiedades 
@@ -20,6 +20,9 @@
  *: Ult.Modif.    :
  *:  Fecha      Modificó            Modificacion
  *:=============================================================================
+ *: 22/Feb/2015 FGil                - Se creo el atributo numLinea y sus metodos set/get.
+ *:                                 Servira para guardar en qué linea se reconoció 
+ *:                                 el token correspondiente.
  *:-----------------------------------------------------------------------------
  */
 
@@ -33,6 +36,7 @@ public class Linea_BE{
         protected String complex;
         protected String lexema;
         protected int entrada;    
+        protected int numLinea;
          
 /*------------------------------------------------------------------------------------------------------------*/
 //Constructor de Linea_BE que no recibe ningun argumento     
@@ -41,18 +45,19 @@ public class Linea_BE{
         {
             complex = "";
             lexema = "";
-            entrada = 0;           
+            entrada = 0;      
+            numLinea = 0;
         }
 /*------------------------------------------------------------------------------------------------------------*/
 //Constructor de Linea_BE con los argumentos necesarios para ser utilizados en nuestra clase BufferEntrada
         
-        public Linea_BE(String compleX, String lexemA, int entradA)
+        public Linea_BE(String compleX, String lexemA, int entradA, int numLineA )
         {
             
             complex = compleX;
             lexema = lexemA;
             entrada = entradA;
-            
+            numLinea = numLineA;
         }
 /*------------------------------------------------------------------------------------------------------------*/
 //Metodo set Complex 
@@ -109,6 +114,19 @@ public class Linea_BE{
             
         }
 /*------------------------------------------------------------------------------------------------------------*/
+//     FGil: Metodo set para numLinea
+        
+       public void setNumLinea ( int numLinea ) {
+           this.numLinea = numLinea;
+       }
+       
+/*------------------------------------------------------------------------------------------------------------*/
+//     FGil: Metodo get para numLinea
+       
+       public int getNumLinea () {
+           return numLinea;
+       }
+       
+/*------------------------------------------------------------------------------------------------------------*/
       
-
 }
